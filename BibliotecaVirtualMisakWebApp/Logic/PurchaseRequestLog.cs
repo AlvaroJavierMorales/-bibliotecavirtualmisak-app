73f0f014306c697ef4c08b6500615f307729e667
DanielaCharcopa <charcopadaniela178@gmail.com>
2024-10-28 11:26:33 -0500

    Se agrega la clase PurchaseRequest a la capa logica

---
+++ BibliotecaVirtualMisakWebApp/Logic/PurchaseRequestLog.cs
@@ -0,0 +1,47 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Logic
+{
+    public class PurchaseRequestLog
+    {
+        PurchaseRequestDat objPur = new PurchaseRequestDat();
+
+
+        // Metodo para mostrar
+        public DataSet showPurchaseRequest()
+        {
+            return objPur.showPurchaseRequest();
+
+        }
+
+        //Metodo DDL
+        public DataSet showPurchaseRequestDDL()
+        {
+            return objPur.showPurchaseRequestDDL();
+
+        }
+
+        // Metodo para guardar
+        public bool savePurchaserequest(string _v_solic_ticket, DateTime _v_solic_fecha, int _v_tbl_usu_id)
+        {
+            return objPur.savePurchaserequest(_v_solic_ticket, _v_solic_fecha, _v_tbl_usu_id);
+        }
+
+        // Metodo para actualizar 
+        public bool updatePurchaserequest(int _v_solic_id, string _v_solic_ticket, DateTime _v_solic_fecha, int _v_tbl_usu_id)
+        {
+            return objPur.updatePurchaserequest(_v_solic_id, _v_solic_ticket, _v_solic_fecha, _v_tbl_usu_id);
+        }
+
+        // Metodo para eliminar
+        public bool deletePurchaserequest(int _v_solic_id)
+        {
+            return objPur.deletePurchaserequest(_v_solic_id);
+        }
+    }
+}
