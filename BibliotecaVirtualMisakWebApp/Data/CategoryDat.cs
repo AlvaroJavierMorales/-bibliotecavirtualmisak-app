7d32cebcecfd0c689cd7d527c38ec586dd0e88ca
sergio ortiz <yhersua08@gmail.com>
2024-10-28 10:48:33 -0500

    se agrega el metodo DDL en la capa de datos categoria

--- BibliotecaVirtualMisakWebApp/Data/CategoryDat.cs
+++ BibliotecaVirtualMisakWebApp/Data/CategoryDat.cs
@@ -25,6 +25,22 @@ namespace Data
             return objData;
         }
 
+        // Método para mostrar id y nombre de Categorías
+        public DataSet showCategoriesDDL()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "procSelectCategoryDDL"; // Nombre actualizado del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
         // Método para guardar una nueva Categoría
         public bool saveCategory(string _nombre, string _description)
         {
