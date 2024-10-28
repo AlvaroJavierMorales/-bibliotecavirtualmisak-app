58f6c225ff34ef4447974a737a275bd1ab01379b
Alvaro Morales <moralestombe19@gmail.com>
2024-10-28 12:40:02 -0500

    Se agrega la clase autores y material educativo (muchos a muchos) a la capa lógica

---
+++ BibliotecaVirtualMisakWebApp/Logic/MaterialAuthorsLogic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using Data;
+
+namespace Logic
+{
+    public class MaterialAuthorsLogic
+    {
+        MaterialAuthorsDat objMaterialAuthorsDat = new MaterialAuthorsDat();
+
+        // Método para mostrar todos los registros de la relación entre Materiales Educativos y Autores
+        public DataSet showMaterialAuthors()
+        {
+            return objMaterialAuthorsDat.showMaterialAuthors();
+        }
+
+        // Método para insertar un nuevo registro en la relación entre Materiales Educativos y Autores
+        public bool saveMaterialAuthor(int autorId, int materialId)
+        {
+            return objMaterialAuthorsDat.saveMaterialAuthor(autorId, materialId);
+        }
+
+        // Método para actualizar un registro de la relación entre Materiales Educativos y Autores
+        public bool updateMaterialAuthor(int autorId, int materialId)
+        {
+            return objMaterialAuthorsDat.updateMaterialAuthor(autorId, materialId);
+        }
+
+        // Método para eliminar un registro de la relación entre Materiales Educativos y Autores
+        public bool deleteMaterialAuthor(int autorId, int materialId)
+        {
+            return objMaterialAuthorsDat.deleteMaterialAuthor(autorId, materialId);
+        }
+    }
+}
