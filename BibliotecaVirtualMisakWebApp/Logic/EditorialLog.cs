5622314c4ec5583267420ace77506c19d9dbfa23
sergio ortiz <yhersua08@gmail.com>
2024-10-28 12:23:20 -0500

    se agrega la clase editorial en la capa logica con sus metodos

---
+++ BibliotecaVirtualMisakWebApp/Logic/EditorialLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data;
+namespace Logic
+{
+    public class EditorialLog
+    {
+        namespace Logic
+    {
+        public class EditorialLog
+        {
+            EditorialDat objEdit = new EditorialDat();
+
+            // Método para mostrar todas las Editoriales
+            public DataSet showEditorials()
+            {
+                return objEdit.showEditorials();
+            }
+
+            // Método para guardar una nueva Editorial
+            public bool saveEditorial(string _nombre, string _ciudad, int _telefono, string _correo)
+            {
+                return objEdit.saveEditorial(_nombre, _ciudad, _telefono, _correo);
+            }
+
+            // Método para actualizar una Editorial
+            public bool updateEditorial(int _idEditorial, string _nombre, string _ciudad, int _telefono, string _correo)
+            {
+                return objEdit.updateEditorial(_idEditorial, _nombre, _ciudad, _telefono, _correo);
+            }
+
+            // Método para eliminar una Editorial
+            public bool deleteEditorial(int _idEditorial)
+            {
+                return objEdit.deleteEditorial(_idEditorial);
+            }
+        }
+    }
+
+}
+}
