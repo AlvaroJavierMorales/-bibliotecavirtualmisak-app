c9f3834c450635cdcd8a901d58ef61177a639299
sergio ortiz <yhersua08@gmail.com>
2024-10-28 12:58:01 -0500

    se agrega la clase editorial en la capa logica con sus metodos

--- BibliotecaVirtualMisakWebApp/Logic/EditorialLog.cs
+++ BibliotecaVirtualMisakWebApp/Logic/EditorialLog.cs
@@ -2,42 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 using Data;
 namespace Logic
 {
     public class EditorialLog
     {
-        namespace Logic
-    {
-        public class EditorialLog
+        EditorialDat objEdit = new EditorialDat();
+
+        // Método para mostrar todas las Editoriales
+        public DataSet showEditorials()
         {
-            EditorialDat objEdit = new EditorialDat();
+            return objEdit.showEditorials();
+        }
 
-            // Método para mostrar todas las Editoriales
-            public DataSet showEditorials()
-            {
-                return objEdit.showEditorials();
-            }
+        // Método para mostrar las editoriales en formato DDL
+        public DataSet showEditorialsDDL()
+        {
+            return objEdit.showEditorialsDDL(); 
 
-            // Método para guardar una nueva Editorial
-            public bool saveEditorial(string _nombre, string _ciudad, int _telefono, string _correo)
-            {
-                return objEdit.saveEditorial(_nombre, _ciudad, _telefono, _correo);
-            }
+        // Método para guardar una nueva Editorial
+        public bool saveEditorial(string _nombre, string _ciudad, int _telefono, string _correo)
+        {
+            return objEdit.saveEditorial(_nombre, _ciudad, _telefono, _correo);
+        }
 
-            // Método para actualizar una Editorial
-            public bool updateEditorial(int _idEditorial, string _nombre, string _ciudad, int _telefono, string _correo)
-            {
-                return objEdit.updateEditorial(_idEditorial, _nombre, _ciudad, _telefono, _correo);
-            }
+        // Método para actualizar una Editorial
+        public bool updateEditorial(int _idEditorial, string _nombre, string _ciudad, int _telefono, string _correo)
+        {
+            return objEdit.updateEditorial(_idEditorial, _nombre, _ciudad, _telefono, _correo);
+        }
 
-            // Método para eliminar una Editorial
-            public bool deleteEditorial(int _idEditorial)
-            {
-                return objEdit.deleteEditorial(_idEditorial);
-            }
+        // Método para eliminar una Editorial
+        public bool deleteEditorial(int _idEditorial)
+        {
+            return objEdit.deleteEditorial(_idEditorial);
         }
     }
-
-}
 }
