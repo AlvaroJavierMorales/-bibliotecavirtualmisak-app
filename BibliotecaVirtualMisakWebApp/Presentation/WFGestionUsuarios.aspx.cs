cbb00780a5ac0419cc163f47495f1d7a1206b011
Alvaro Morales <moralestombe19@gmail.com>
2024-11-13 12:59:35 -0500

    Se agrega la función de la gestión de usuarios

--- BibliotecaVirtualMisakWebApp/Presentation/WFGestionUsuarios.aspx.cs
+++ BibliotecaVirtualMisakWebApp/Presentation/WFGestionUsuarios.aspx.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Logic;
 
 namespace Presentation
 {
