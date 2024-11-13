035809c9dd8236c58b30b383f1eaedb98a7b2a9e
Alvaro Morales <moralestombe19@gmail.com>
2024-11-13 11:58:03 -0500

    Se agregan el formulario y el árrea de trabajo de la funcion

---
+++ BibliotecaVirtualMisakWebApp/Presentation/WFGestionUsuarios.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFGestionUsuarios.aspx.cs" Inherits="Presentation.WFGestionUsuarios" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+       <div>
+        <h2>Modificar Rol de Usuario</h2>
+
+        <asp:HiddenField ID="HFUserId" runat="server" />
+        <br />
+        <asp:Label ID="LblFirstName" runat="server" AssociatedControlID="TBFirstName" Text="Nombre:" />
+        <asp:TextBox ID="TBFirstName" runat="server" />
+
+        <asp:Label ID="LblLastName" runat="server" AssociatedControlID="TBLastName" Text="Apellido:" />
+        <asp:TextBox ID="TBLastName" runat="server" />
+        <br /> <br />
+        <asp:Label ID="LblEmail" runat="server" AssociatedControlID="TBEmail" Text="Correo electrónico:" />
+        <asp:TextBox ID="TBEmail" runat="server" />
+        <br /> <br />
+        <asp:Label ID="LblPassword" runat="server" AssociatedControlID="TBPassword" Text="Contraseña:" />
+        <asp:TextBox ID="TBPassword" runat="server" TextMode="Password" />
+        <br /> <br />
+        <asp:Label ID="LblSalt" runat="server" AssociatedControlID="TBSalt" Text="Salt para la contraseña:" />
+        <asp:TextBox ID="TBSalt" runat="server" />
+        <br /> <br />
+
+        <!-- Cambiamos el campo de rol a DropDownList con roles específicos -->
+        <asp:Label ID="LblRol" runat="server" AssociatedControlID="DDLRole" Text="Rol del usuario:" />
+        <asp:DropDownList ID="DDLRole" runat="server">
+            <asp:ListItem Value="" Text="Selecciona un rol" />
+            <asp:ListItem Value="Administrador" Text="Administrador" />
+            <asp:ListItem Value="Docente" Text="Docente" />
+            <asp:ListItem Value="Estudiante" Text="Estudiante" />
+        </asp:DropDownList>
+        <br /> <br />
+
+        <!-- Cambiamos el campo de nivel de estudios a DropDownList -->
+      <asp:Label ID="LblEducationLevel" runat="server" AssociatedControlID="DDLEducationLevel" Text="Nivel de estudios:" />
+<asp:DropDownList ID="DDLEducationLevel" runat="server">
+    <asp:ListItem Value="" Text="Selecciona un nivel de estudios" />
+    <asp:ListItem Value="Primaria" Text="Primaria" />
+    <asp:ListItem Value="Secundaria" Text="Secundaria" />
+    <asp:ListItem Value="Bachillerato" Text="Bachillerato" />
+    <asp:ListItem Value="Técnico" Text="Técnico" />
+    <asp:ListItem Value="Tecnólogo" Text="Tecnólogo" />
+    <asp:ListItem Value="Pregrado" Text="Pregrado" />
+    <asp:ListItem Value="Especialización" Text="Especialización" />
+    <asp:ListItem Value="Maestría" Text="Maestría" />
+    <asp:ListItem Value="Doctorado" Text="Doctorado" />
+    <asp:ListItem Value="Postdoctorado" Text="Postdoctorado" />
+</asp:DropDownList>
+<br /> <br />
+
+        <br /> <br />
+
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" OnClick="BtnUpdate_Click" />
+        <asp:Button ID="BtnDelete" runat="server" Text="Eliminar" OnClick="BtnDelete_Click" />
+
+        <asp:Label ID="LblMessage" runat="server" Text=""></asp:Label>
+    </div>
+
+   <div>
+    <asp:GridView ID="GVUsers" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GVUsers_SelectedIndexChanged">
+        <Columns>
+            <asp:BoundField DataField="usu_id" HeaderText="ID" />
+           <asp:BoundField DataField="usu_nombre" HeaderText="Nombre" />
+
+    <asp:BoundField DataField="usu_apellido" HeaderText="Apellido" />
+    <asp:BoundField DataField="usu_correo" HeaderText="Correo electrónico" />
+    <asp:BoundField DataField="usu_rol" HeaderText="Rol" />      <%--Nueva columna para el rol --%>
+    <asp:BoundField DataField="usu_nivel_estudios" HeaderText="Nivel de estudios" />    <%--Nueva columna para el nivel de estudios--%>
+    <asp:CommandField HeaderText="Opción" ShowSelectButton="True" />
+<%--           <asp:CommandField ShowDeleteButton="True" />--%>
+        </Columns>
+    </asp:GridView>
+</div>
+</asp:Content>
---
+++ BibliotecaVirtualMisakWebApp/Presentation/WFGestionUsuarios.aspx.designer.cs
@@ -0,0 +1,197 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentation
+{
+
+
+    public partial class WFGestionUsuarios
+    {
+
+        /// <summary>
+        /// Control HFUserId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HFUserId;
+
+        /// <summary>
+        /// Control LblFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblFirstName;
+
+        /// <summary>
+        /// Control TBFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBFirstName;
+
+        /// <summary>
+        /// Control LblLastName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblLastName;
+
+        /// <summary>
+        /// Control TBLastName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBLastName;
+
+        /// <summary>
+        /// Control LblEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblEmail;
+
+        /// <summary>
+        /// Control TBEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBEmail;
+
+        /// <summary>
+        /// Control LblPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblPassword;
+
+        /// <summary>
+        /// Control TBPassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBPassword;
+
+        /// <summary>
+        /// Control LblSalt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblSalt;
+
+        /// <summary>
+        /// Control TBSalt.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBSalt;
+
+        /// <summary>
+        /// Control LblRol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblRol;
+
+        /// <summary>
+        /// Control DDLRole.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLRole;
+
+        /// <summary>
+        /// Control LblEducationLevel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblEducationLevel;
+
+        /// <summary>
+        /// Control DDLEducationLevel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLEducationLevel;
+
+        /// <summary>
+        /// Control BtnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSave;
+
+        /// <summary>
+        /// Control BtnUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnUpdate;
+
+        /// <summary>
+        /// Control BtnDelete.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnDelete;
+
+        /// <summary>
+        /// Control LblMessage.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblMessage;
+
+        /// <summary>
+        /// Control GVUsers.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVUsers;
+    }
+}
